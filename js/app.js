5ab4cd1df6dc7071b420a2efd8fc9ae51deb744b
Nematov Ro'zimuhammad <jeki2004114r@gmail.com>
2024-06-27 14:00:47 +0500

    main

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 let btn = document.querySelector(".toggle")
+let btn2 = document.querySelector(".toggle2")
 
 
 btn.addEventListener( 'click', () =>{    document.querySelector(".sidebar").classList.toggle("active")
+})
+
+btn2.addEventListener( 'click', () =>{    document.querySelector(".sidebar").classList.toggle("active")
 })
